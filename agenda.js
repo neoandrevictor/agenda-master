33eefde8e5663935b9bf1f3602beb2eca3fcc78d
neoandrevictor <neoandrevictor@hotmail.com>
2019-02-23 15:58:27 -0300

    Add files via upload

--- agenda.js
+++ agenda.js
@@ -4,14 +4,16 @@
 
         fields:document.querySelectorAll("input"),
         buttom:document.querySelector(".pure-button"),
-        table:document.querySelector("table")
+        table:document.querySelector("table tbody")
 
 
 
     
     };
     //actions
+    var cleanFields=()=> ui.fields.forEach(field=>field.value="");
 
+    var genericError=function(){};
     var validateFields=function(e){
         e.preventDefault();
         var errors=0;
@@ -48,17 +50,55 @@
             })
         }
 
-        fetch(endpoint,config);
+        fetch(endpoint,config)
+        .then(cleanFields)
+        .then(getContacts)
+        .catch(genericError);
     };
 
-    var getContacts=function(){};
+
+
+    var getContacts=function(){
+        var endpoint="http://localhost:3000/schedule";
+        var config={
+            method:"GET",
+            
+            headers:new Headers({
+                "content-type":"application/json"
+            })
+        }
+
+        fetch(endpoint,config)
+        .then(function(response){return response.json()})
+        .then(getContactsSuccess)
+        .catch(genericError);
+    };
 
     var removeContact=function(){};
+
+    var getContactsSuccess=function(contacts){
+        var tableRows=[];
+        contacts.forEach(function(contact){
+            tableRows.push(`
+                <tr>
+                    <td>${contact.id}</td>
+                    <td>${contact.name}</td>
+                    <td>${contact.email}</td>
+                    <td>${contact.phone}</td>
+                </tr>          
+            
+            `)
+        })
+        
+        ui.table.innerHTML=tableRows.join("");
+        
+    }
 
     var init=function(){
 
         //adds events
         ui.buttom.onclick=validateFields;
+        getContacts();
        
 
     }();
