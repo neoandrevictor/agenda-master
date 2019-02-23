9a59e6ca652eaac18cc7d5af8d2420248cf5b83a
neoandrevictor <neoandrevictor@hotmail.com>
2019-02-23 12:48:59 -0300

    Add files via upload

--- agenda.js
+++ agenda.js
@@ -1,101 +1,69 @@
+(function (){
 
-form=document.getElementsByClassName("pure-form pure-form-aligned")[0];
-form.addEventListener("submit", adicionar, true);
-let tabela=document.getElementsByClassName("pure-table")[0];
-tabela.lastElementChild.lastElementChild.lastElementChild.outerHTML="";
-coluna=document.createElement("th");
-coluna.innerText="Ações";
-tabela.children[0].children[0].appendChild(coluna);
-let total=localStorage.getItem('contador')*1;
-var i;
-if(total!=null || total!=""){
-    for(i=1;i<=total;i++){
+    var ui={
 
-        adicionar(localStorage.getItem(i));
-    }
-}
+        fields:document.querySelectorAll("input"),
+        buttom:document.querySelector(".pure-button"),
+        table:document.querySelector("table")
 
-document.getElementById("name").setAttribute("required",true);
-document.getElementById("email").setAttribute("required",true);
-document.getElementById("email").setAttribute("type","email");
 
-function adicionar(codigo){
 
-    if (typeof(codigo)=="object"){
-        if (codigo!=null){
-            event.preventDefault();
-        
-        
+    
+    };
+    //actions
+
+    var validateFields=function(e){
+        e.preventDefault();
+        var errors=0;
+        var contact={};
+        console.log(ui.fields);
+
+        ui.fields.forEach(function(field){
+            if (field.value.trim().length===0){
+                field.classList.add("error");
+                errors++;
+            } else {
+                field.classList.remove("error");
+                contact[field.id]=field.value.trim();
+            }
+        })
+
+        console.log(errors,contact);
+
+        if (errors>0){
+            document.querySelector(".error").focus();
         } else {
-            return;
+            addContact(contact);
         }
-    }
-    let nome=document.getElementById("name").value;
-    let email=document.getElementById("email").value;
-    let contador=localStorage.getItem("contador")*1;
-    contador++;
-    let linha=document.createElement("tr");
+
+    };
+
+    var addContact=function(contact){
+        var endpoint="http://localhost:3000/schedule";
+        var config={
+            method:"POST",
+            body:JSON.stringify(contact),
+            headers:new Headers({
+                "content-type":"application/json"
+            })
+        }
+
+        fetch(endpoint,config);
+    };
+
+    var getContacts=function(){};
+
+    var removeContact=function(){};
+
+    var init=function(){
+
+        //adds events
+        ui.buttom.onclick=validateFields;
+       
+
+    }();
+
+    console.log(ui);
+
     
-    
-    if (typeof(codigo)=="object"){
-        linha.id=`registro_${contador}`
-        codigo=`
-        <td>${contador}</td>
-        <td>${nome}</td>
-        <td>${email}</td>
-        <td style='cursor:pointer' id='excluir_${contador}' onclick='excluir(this.id)'>Excluir</td>`;
-        localStorage.setItem("contador",contador);
-        localStorage.setItem(contador,codigo);
-
-    } else {
-
-        linha.id="registro_"+i;
-    }
-    linha.innerHTML=codigo;
-    
-    let tabela=document.getElementsByClassName("pure-table")[0];
-    tabela.appendChild(linha);
-    
-
-
-}
-
-
-
-
-function excluir(id){
-    
-    numeroregistro=id.replace("excluir_","");
-    idregistro="registro_"+numeroregistro;
-    document.getElementById(idregistro).outerHTML="";
-    localStorage.removeItem(numeroregistro);
-
-
-}
-
-function ajaxPost(data){
-
-    var request = new XMLHttpRequest();
-    request.open('POST', '/my/url', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(data);
-}
-
-function ajaxDelete(data){
-
-    var request = new XMLHttpRequest();
-    request.open('DELETE', '/my/url', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(data);
-}
-
-function ajaxGet(){
-
-    fetch('http://localhost:3000/agenda', {
-        method: 'GET',
-        headers: {'Content-Type':'application/x-www-form-urlencoded'}
-    })
-    .then(response => ajaxPega=response)
-
-}
-
+})();
